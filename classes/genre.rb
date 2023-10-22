19df79750f15ebf12a21bee0be302ffb905e5f11
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-10-22 05:42:23 +0300

    rewrite all classes to meet requirements

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,17 +1,23 @@
-require_relative '../item'
-
 class Genre
-  attr_accessor :name
-  attr_reader :id, :items
+  attr_accessor :name, :id
+  attr_reader :items
 
   def initialize(name)
     @id = Random.rand(1...1000)
     @name = name
+
     @items = []
   end
 
   def add_item(item)
     @items << item
     item.genre = self
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'name' => @name
+    }
   end
 end
--- classes/item.rb
+++ classes/item.rb
@@ -1,38 +1,51 @@
 require 'date'
 
 class Item
-  attr_accessor :id, :publish_date
-  attr_reader :archived, :genre, :author, :label
+  attr_accessor :id, :genre, :author, :source, :label, :publish_date
+  attr_reader :archived
 
-  def initialize(publish_date, archived: false, id: Random.rand(1...1000))
-    @id = id
-    @publish_date = Date.parse(publish_date)
+  def initialize(publish_date, archived)
+    unless publish_date.is_a?(Date)
+      raise ArgumentError,
+            "Invalid date parameter: #{publish_date}. publish_date must be a Date object."
+    end
+
+    @id = Random.rand(1..10_000)
+    @publish_date = publish_date || Date.today
     @archived = archived
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
   end
 
-  def genre=(genre)
-    @genre = genre
-    genre.items << self unless genre.items.include?(self)
-  end
-
-  def author=(author)
-    @author = author
-    author.items << self unless author.items.include?(self)
-  end
-
-  def label=(label)
-    @label = label
-    label.items << self unless label.items.include?(self)
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) > 10
   end
 
   def move_to_archive
-    @archived = true if can_be_archived?
+    return unless can_be_archived?
+
+    @archived = true
+  end
+
+  def to_hash
+    {
+      'id' => @id,
+      'publish_date' => @publish_date,
+      'archived' => @archived,
+      'genre' => extract_id(genre),
+      'author' => extract_id(author),
+      'source' => extract_id(source),
+      'label' => extract_id(label)
+    }
   end
 
   private
 
-  def can_be_archived?()
-    difference = Time.now.year - @publish_date.year.to_i
-    difference > 10
+  def extract_id(item)
+    return nil if item.nil?
+
+    item.id
   end
 end
