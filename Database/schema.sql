dfd99743f3a9548d567cd7025cdbfdd3cbf18fbb
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-10-22 05:39:04 +0300

    Upgrade schema

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,59 +1,57 @@
-CREATE TABLE Item (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY
+CREATE TABLE labels(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(50),
+    color VARCHAR(50)
 );
 
-CREATE TABLE Book (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INTEGER,
-  publish_date DATE,
-  archived BOOLEAN,
-  publisher VARCHAR(150),
-  cover_state VARCHAR(200),
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE genres(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50)
 );
 
-CREATE TABLE Labels (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INTEGER,
-  title VARCHAR(150),
-  color VARCHAR(100),
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE authors(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
 );
 
-CREATE TABLE Author (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  item_id INTEGER,
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE items(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE,
+    archived boolean,
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id),
+    label_id INT REFERENCES labels(id)
 );
 
-CREATE TABLE Game (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INTEGER,
-  last_played_date DATE NOT NULL,
-  multiplayer BOOLEAN NOT NULL,
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE books(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publisher VARCHAR(50),
+    cover_state VARCHAR(50),
+    publish_date DATE,
+    archived boolean,
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id),
+    label_id INT REFERENCES labels(id)
 );
 
-CREATE TABLE MusicAlbum (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  item_id INTEGER,
-  on_spotify BOOLEAN,
-  publish_date DATE,
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE musicalbums(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    on_spotify BOOLEAN,
+    publish_date DATE,
+    archived boolean,
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id),
+    label_id INT REFERENCES labels(id)
 );
 
-CREATE TABLE Genre (
-  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  name VARCHAR(150),
-  item_id INTEGER,
-  FOREIGN KEY(item_id) REFERENCES Item(id)
+CREATE TABLE games(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    multiplayer BOOLEAN,
+    last_played_at DATE,
+    publish_date DATE,
+    archived boolean,
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id),
+    label_id INT REFERENCES labels(id)
 );
-
-CREATE INDEX index_music_album_item_id ON MusicAlbum(item_id);
-CREATE INDEX index_genre_item_id ON Genre(item_id);
-CREATE INDEX index_game_item_id ON Game(item_id);
-CREATE INDEX index_book_item_id ON Book(item_id);
-CREATE INDEX index_labels_item_id ON Labels(item_id);
-CREATE INDEX index_author_item_id ON Author(item_id);
